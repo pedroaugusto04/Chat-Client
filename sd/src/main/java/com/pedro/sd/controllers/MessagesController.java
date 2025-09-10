b35dc782c06eff94e5da943471a02534cbe0379b
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2025-09-10 17:34:30 -0300

    feat: inicio adaptacao para envio de mensagens websocket

--- sd/src/main/java/com/pedro/sd/controllers/MessagesController.java
+++ sd/src/main/java/com/pedro/sd/controllers/MessagesController.java
@@ -1,22 +1,20 @@
 package com.pedro.sd.controllers;
 
+import com.pedro.sd.models.DTO.MessageResponseDTO;
+import com.pedro.sd.models.DTO.MessageSendDTO;
+import com.pedro.sd.models.DTO.UserDTO;
+import com.pedro.sd.services.LogsService;
+import com.pedro.sd.services.MessagesService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.pedro.sd.models.DTO.MessageResponseDTO;
-import com.pedro.sd.models.DTO.MessageSendDTO;
-import com.pedro.sd.services.MessagesService;
-
-import com.pedro.sd.services.LogsService;
 
 
 @RestController
@@ -64,5 +62,35 @@ public class MessagesController {
         this.logsService.log(messages, "GET_MESSAGES", "Recuperou com sucesso as mensagens do grupo " + groupId + " em " + latency + " ms");
 
         return ResponseEntity.ok(messages);
+    }
+
+    @MessageMapping("/chat/{groupId}")
+    @SendTo("/topic/messages.{groupId}")
+    public void connectToGroup(@DestinationVariable Long groupId, UserDTO userDTO) {
+        this.logsService.log(userDTO, "GROUP_CONNECT", userDTO.nickname() + " conectou no grupo WEB SOCKET " + groupId);
+    }
+
+    @MessageMapping("/chat/{groupId}/send")
+    @SendTo("/topic/messages.{groupId}")
+    public MessageResponseDTO sendMessageWS(@DestinationVariable Integer groupId, MessageSendDTO messageDTO) {
+
+        this.logsService.log(messageDTO, "SEND_MESSAGE_WS",
+                "Entrou no endpoint WS para envio de mensagem");
+
+        long startTime = System.currentTimeMillis();
+
+        messagesService.sendMessage(groupId, messageDTO);
+
+        long latency = System.currentTimeMillis() - startTime;
+
+        this.logsService.log(messageDTO, "SEND_MESSAGE_WS",
+                "Mensagem enviada via WEB SOCKET em " + latency + " ms");
+
+        return new MessageResponseDTO(
+                messageDTO.text(),
+                null,
+                messageDTO.userNickname(),
+                messageDTO.timestampClient()
+        );
     }
 }
